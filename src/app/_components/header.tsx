a297886a32418d6a635c00eae16a2efbfff42067
dhimiterPoison <dhimiter.helmi@gmail.com>
2024-10-07 01:35:18 +0200

    changed copy, added sort

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -6,7 +6,7 @@ import SpotModal from './spotModal'
 
 const Header = () => {
   return (
-	<div className='flex items-center h-20 justify-around'>
+	<div className=' w-full flex items-center h-20 px-12 justify-between'>
 		<span className='font-bold text-3xl'>Spotted</span>
 		<div className='flex gap-4'>
                 {/* {session && <span>Logged in as {session.user?.name}</span>} */}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,68 @@
-import Link from "next/link";
+import { pgTable, text } from 'drizzle-orm/pg-core'
+import { api, HydrateClient } from '~/trpc/server'
+import { getXataClient } from '~/xata'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from './_components/ui/card'
 
-import { LatestPost } from "~/app/_components/post";
-import { getServerAuthSession } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./_components/ui/card";
-import { Button } from "./_components/ui/button";
-import { LatestPosts } from "./_components/homeposts";
-import { getXataClient } from "~/xata";
-import { pgTable, text } from "drizzle-orm/pg-core";
-import SpotModal from "./_components/spotModal";
-
-
-const xata = getXataClient();
+const xata = getXataClient()
 const spots = pgTable('spots', {
-  id: text('id').primaryKey(),
-  name: text('content'),
-  city: text('location')
-});
+    id: text('id').primaryKey(),
+    name: text('content'),
+    city: text('location'),
+})
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  // const session = await getServerAuthSession();
+    const hello = await api.post.hello({ text: 'from tRPC' })
+    // const session = await getServerAuthSession();
 
-  // void api.post.getLatest.prefetch();
+    const spotsList = await xata.db.spots
+        .select(['xata.createdAt', 'content', 'location'])
+        .getPaginated({
+            pagination: {
+                size: 16,
+            },
+            sort: {
+                column: 'xata.createdAt',
+                direction: 'desc',
+            },
+        })
 
+    return (
+        <HydrateClient>
+            <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#e8d7ff] to-[#15162c] text-white">
+                <div className="container grid grid-cols-2 md:grid-cols-3  gap-4 p-4 w-full h-full">
+                    <div className="col-span-2  text-black p-6">
+                        <h1 className="text-4xl font-bold mb-4">
+                            Welcome to Spotted
+                        </h1>
+                        <p className="text-lg">
+                            Send a smile to someone! Did you appreciate
+                            someone's style? Did you see someone doing something
+                            cool? Tell them!
+                        </p>
+                    </div>
 
-  const spotsList = await xata.db.spots.select(['content', 'location']).getPaginated({
-    pagination: {
-      size: 16,
-    },
-  });
-  
-
-  return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#e8d7ff] to-[#15162c] text-white">
-          <div className="container grid grid-cols-3  gap-4 p-4 w-full h-full">
-            <div className="col-span-2  text-black p-6">
-              <h1 className="text-4xl font-bold mb-4">Welcome to Spotted</h1>
-              <p className="text-lg">Connect with people you've seen around campus. Make new friends and expand your network!</p>
-            </div>
-
-
-                {spotsList.records.map((spot) => (
-                  <div  key={spot.id} className="col-span-1 row-span-1">
-                  <Card className="h-full">
-                    <CardHeader>
-                      <CardDescription>{spot.location}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                      <CardTitle>{spot.content}</CardTitle>
-                    </CardContent>
-                  </Card></div>
-                ))}
-              {/* <Card className="h-full">
+                    {spotsList.records.map(spot => (
+                        <div key={spot.id} className="col-span-1 row-span-1">
+                            <Card className="h-full">
+                                <CardHeader>
+                                    <CardDescription>
+                                        {spot.location}
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <CardTitle>{spot.content}</CardTitle>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    ))}
+                </div>
+                {/* <Card className="h-full">
                 <CardHeader>
                   <CardTitle>Get Started</CardTitle>
                   <CardDescription>Create your profile or search for others</CardDescription>
@@ -63,10 +72,8 @@ export default async function Home() {
                   <Button variant="outline" className="w-full">Search Users</Button>
                 </div>
               </Card> */}
-            </div>
-            <div>
-            </div>
-          {/* <div className="flex flex-col items-center gap-2">
+                <div></div>
+                {/* <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
             </p>
@@ -78,8 +85,8 @@ export default async function Home() {
             </div>
           </div> */}
 
-          {/* {session?.user && <LatestPost />} */}
-      </main>
-    </HydrateClient>
-  );
+                {/* {session?.user && <LatestPost />} */}
+            </main>
+        </HydrateClient>
+    )
 }
